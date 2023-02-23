e4af5bf31ba4443b5f697647bb00a69ea4c20356
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-23 14:54:36 -0300

    Aplicando JavaScript na transição dos cards

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,6 +1,20 @@
+function handleMouseEnter() {
+  this.classList.add('s-card--hovered')
+}
+
+function handleMouseLeave () {
+  this.classList.remove('s-card--hovered');
+}
+
 function addEventListenersToCards() {
   const cardElements = document.getElementsByClassName('s-card');
   console.log(cardElements);
+
+  for (let index = 0; index < cardElements.length; index++) {
+    const card = cardElements[index];
+    card.addEventListener('mouseenter', handleMouseEnter);
+    card.addEventListener('mouseleave', handleMouseLeave);
+  }
 }
 
 
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,12 @@
-function handleMouseEnter() {
-  this.classList.add('s-card--hovered');
-  document.body.id = `${this.id}-hovered`;
-}
+// function handleMouseEnter() {
+//   this.classList.add('s-card--hovered');
+//   document.body.id = `${this.id}-hovered`;
+// }
 
-function handleMouseLeave() {
-  this.classList.remove('s-card--hovered');
-  document.body.id = '';
-}
+// function handleMouseLeave() {
+//   this.classList.remove('s-card--hovered');
+//   document.body.id = '';
+// }
 
 // function addEventListenersToCards() {
 //   const cardElements = document.getElementsByClassName('s-card');
@@ -18,19 +18,19 @@ function handleMouseLeave() {
 //   }
 // }
 
-document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+// document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
 
-function selectCarouselItem(selectedButtonElement) {
-  const selectedItem = selectedButtonElement.id;
-  const carousel = document.querySelector('.s-cards-carousel');
-  const transform = carousel.style.transform;
-  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
-  const rotateYDeg = -120 * (Number(selectedItem) - 1);
-  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+// function selectCarouselItem(selectedButtonElement) {
+//   const selectedItem = selectedButtonElement.id;
+//   const carousel = document.querySelector('.s-cards-carousel');
+//   const transform = carousel.style.transform;
+//   const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+//   const rotateYDeg = -120 * (Number(selectedItem) - 1);
+//   const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
 
-  carousel.style.transform = newTransform;
+//   carousel.style.transform = newTransform;
 
-  const activeButtonElement = document.querySelector('.s-controller__button--active');
-  activeButtonElement.classList.remove('s-controller__button--active');
-  selectedButtonElement.classList.add('s-controller__button--active');
-}
+//   const activeButtonElement = document.querySelector('.s-controller__button--active');
+//   activeButtonElement.classList.remove('s-controller__button--active');
+//   selectedButtonElement.classList.add('s-controller__button--active');
+// }
