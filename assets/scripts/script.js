0595844e8bb32860b30a97f3da5efdeba0ff4c82
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-01 01:53:47 -0300

    Atribuindo JavaScript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,24 @@
-// function handleMouseEnter() {
-//   this.classList.add('s-card--hovered');
-//   document.body.id = `${this.id}-hovered`;
-// }
+function handleMouseEnter() {
+  this.classList.add('s-card--hovered');
+  document.body.id = `${this.id}-hovered`;
+}
 
-// function handleMouseLeave() {
-//   this.classList.remove('s-card--hovered');
-//   document.body.id = '';
-// }
+function handleMouseLeave() {
+  this.classList.remove('s-card--hovered');
+  document.body.id = '';
+}
 
-// function addEventListenersToCards() {
-//   const cardElements = document.getElementsByClassName('s-card');
+function addEventListenersToCards() {
+  const cardElements = document.getElementsByClassName('s-card');
   
-//   for (let index = 0; index < cardElements.length; index++) {
-//     const card = cardElements[index];
-//     card.addEventListener('mouseenter', handleMouseEnter);
-//     card.addEventListener('mouseleave', handleMouseLeave);
-//   }
-// }
+  for (let index = 0; index < cardElements.length; index++) {
+    const card = cardElements[index];
+    card.addEventListener('mouseenter', handleMouseEnter);
+    card.addEventListener('mouseleave', handleMouseLeave);
+  }
+}
 
-// document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
 
 // function selectCarouselItem(selectedButtonElement) {
 //   const selectedItem = selectedButtonElement.id;
